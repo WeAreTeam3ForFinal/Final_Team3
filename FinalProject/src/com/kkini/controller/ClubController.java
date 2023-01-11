5509aecd99b4d99a1864c1ceeeb971da76b540c5
Neetjun <dudwnsnar@naver.com>
2023-01-11 16:20:11 +0900

    #13 동아리리스트 마이바티스
    1. ClubController.java 수정
    2. IClubDAo.java 수정
    3. ClubDao.xml 수정
    4. ClubListAjax.jsp 수정
    5. main_jyj2.jsp 수정
    
    --- 주요 기능 추가
    1. 동아리 리스트 정렬 기능

--- FinalProject/src/com/kkini/controller/ClubController.java
+++ FinalProject/src/com/kkini/controller/ClubController.java
@@ -1,12 +1,12 @@
 package com.kkini.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.kkini.mybatis.IClubDAO;
 
@@ -18,19 +18,31 @@ public class ClubController
 	private SqlSession sqlSession;
 	
 	@RequestMapping(value = "/clubList.kkini", method = RequestMethod.GET)
-	public String clubList(Model model)
+	public String clubList(Model model, HttpServletRequest request)
 	{
 		
 		String result = "";
 		
+		String sortBy = request.getParameter("sortBy");
+		
+		if(sortBy == null)
+			sortBy = "";
+		
 		try
 		{
-			
 			IClubDAO dao = sqlSession.getMapper(IClubDAO.class);
-			
-			model.addAttribute("clubList", dao.clubList());
+		
+			if(sortBy.equals("date"))
+				model.addAttribute("clubList", dao.sortClubListByDate());
+			else if(sortBy.equals("memUp"))
+				model.addAttribute("clubList", dao.sortClubListByMemUp());
+			else if(sortBy.equals("memDown"))
+				model.addAttribute("clubList", dao.sortClubListByMemDown());
+			else
+				model.addAttribute("clubList", dao.clubList());
 			
 			result = "/ClubListAjax.jsp";
+			
 		} 
 		catch (Exception e)
 		{
